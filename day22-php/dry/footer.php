e047fdad405bcae4b863e784487c4ee1196a3bdb
j_polak <jan.polak@onmyway.cz>
2019-02-05 10:39:03 +0100

    day 22 workout

---
+++ day22-php/dry/footer.php
@@ -0,0 +1,4 @@
+<?php require_once 'functions.php'; ?>
+<footer>
+    © <?= get_current_year() ?> Me, myself &amp; I
+</footer>
---
+++ day22-php/dry/header.php
@@ -0,0 +1,8 @@
+<?php require_once 'functions.php'; ?>
+<header>
+    The best page of <?= get_current_year() ?>
+    <nav>
+        <a href="home.php">Landing page</a>
+        <a href="page.php">Page #2</a>
+    </nav>
+</header>
